7cd681ce33c2a40e4a4aef215155e1fe4ecf1964
qdw77 <qkrdmswn683@gmail.com>
2024-05-30 13:02:15 +0900

    ✨ js

---
+++ box-layout/position/실습1/정답/script.js
@@ -0,0 +1,15 @@
+// Javascript 변수명 (낙타표기법)
+let popup = document.querySelector('.popup');
+let popupOpenBtn = document.querySelector('.popup-open-btn');
+let popupCloseBtn = document.querySelector('.popup-close-btn');
+
+  // function()
+
+  // popupOpenBtn
+popupOpenBtn.addEventListener('click', function(){
+  popup.classList.add('is-active')
+})
+
+popupCloseBtn.addEventListener('click', function(){
+  popup.classList.remove('is-active')
+})
