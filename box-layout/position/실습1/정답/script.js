1a3dea3f829833013171de7b5946a7158ce5b9a3
qdw77 <qkrdmswn683@gmail.com>
2024-05-31 10:59:20 +0900

    💄 close/open

--- box-layout/position/실습1/정답/script.js
+++ box-layout/position/실습1/정답/script.js
@@ -13,3 +13,29 @@ popupOpenBtn.addEventListener('click', function(){
 popupCloseBtn.addEventListener('click', function(){
   popup.classList.remove('is-active')
 })
+
+// sidebar
+let sidebar = document.querySelector('.sidebar')
+let sidebarOpenBtn = document.querySelector('.sidebar-open-btn')
+let sidebarCloseBtn = document.querySelector('.sidebar-close-btn')
+
+// 1.sidebarOpenBtn을 클릭했을 때
+// sidebar에 is-active 클래스 추가
+
+sidebarOpenBtn.addEventListener('click',function(){
+  // sidebarOpenBtn클릭 했을 때 sidebar에
+  // is-active라는 클래스 추가
+  sidebar.classList.add('is-active')
+})
+
+sidebar.addEventListener('click', function(){
+  sidebar.classList.remove('is-active')
+})
+
+// 함수 소괄호 안 : 매개변수
+function plus (num1,num2 ) {
+  console.log(num1, num2)
+  console.log(num1 + num2);
+}
+// 함수는 호출했을 때 실행된다.
+plus(100, 10)
