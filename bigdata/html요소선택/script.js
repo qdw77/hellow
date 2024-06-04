7d88497f225cf24d5a41fed77b7f758874238cad
qdw77 <qkrdmswn683@gmail.com>
2024-06-04 12:58:37 +0900

    ✨ j-Qurey

--- bigdata/html요소선택/script.js
+++ bigdata/html요소선택/script.js
@@ -5,4 +5,17 @@ let changeBtn = document.getElementById('txt-changing-btn');
 hello.innerHTML = '안녕!';
 hello.style.color = 'red';
 
+changeBtn.addEventListener('click', () => {
+  hello.innerHTML = 'Hello';
+  hello.style.color = 'blue';
+})
+  //querySelectorAll은 모든 item들을 다 찾아온다
+let item = document.querySelectorAll('.item');
+
+document.querySelectorAll('.item')[0].style.color = 'blue';
+document.querySelectorAll('.item')[1].style.color = 'blue';
+document.querySelectorAll('.item')[2].style.color = 'blue';
+
+  //j-Qurey
+  $('.item').css('color', 'red');
 
