f0757a1216ae82555385ec9e19f2c80e1860465b
qdw77 <qkrdmswn683@gmail.com>
2024-06-10 09:41:55 +0900

    ✨ input 십습 정답

---
+++ bigdata/태그연습/input 실습1/input실습 정답/script.js
@@ -0,0 +1,13 @@
+let year = document.getElementById('year')
+let month = document.getElementById('month')
+let date = document.getElementById('date')
+
+let kst = new Date();
+let getYear = kst.getFullYear();
+
+// 1900 i 값이 작거나 같을 때까지 i를 1씩 더하며 반복
+for(let i = 1920; i <= getYear; i++) {
+  let option = `<option>${i}</option>`;
+  year.insertAdjacentHTML('beforeend',option);
+}
+
